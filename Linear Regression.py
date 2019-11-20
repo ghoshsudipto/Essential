36488fb290833fa6cd7c466ea4c8620981b1baff
Sudipto Ghosh <ghoshsudipto@users.noreply.github.com>
2019-11-20 17:24:33 +0530

    Update Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,22 +1,37 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
-from scipy.stats.mstats import gmean
 
-pd.set_option('display.max_column', 10)
+pd.set_option('display.max_column', 100)
 pd.set_option('display.max_row', 500)
 pd.set_option('display.width', 500)
 plt.style.use('ggplot')
 
-df = pd.read_csv(r'C:\Users\Sudipto\Dropbox\Python\DA\datasets\BN.csv')
+df = pd.read_csv(r'H:\Py\dataset\BN.csv')
 df[['Date', 'Expiry']] = df[['Date', 'Expiry']].apply(pd.to_datetime)
 df.set_index('Date', inplace=True)
-df['Range'] = df['High'] - df['Low']
-df['CoC'] = df['Close'] - df['Underlying']
-df['AvCoC'] = df['CoC'].rolling(3).apply(gmean, raw=True)
+df['RangeQ'] = (df['High'] - df['Low'])/3
+ # df['High'] - df['Close'] < RangeQ
+
+df['haha'] = np.select([df['High'].shift(1) - df['Close'].shift(1) < df['RangeQ']], [df['Close'].shift(1)-df['Close']])
+# df['CoC'] = df['Close'] - df['Underlying']
+# df['AvCoC'] = df['CoC'].rolling(3).mean()
+
+condition = [(df['Expiry'] == df['Expiry'].shift(-1)) & (df['Expiry'] == df['Expiry'].shift(1)) & (df['Expiry'] == df['Expiry'].shift(2))]
+choice = [df['Close'].rolling(3).mean()]
+df['MA3'] = np.select(condition, choice)
+df['MA3'].replace(0, np.nan, inplace=True)
+
 print(df)
 
-plt.scatter(df['CoC'].shift(1), df['Close'], color='red')
-plt.title('Close vs CoC')
-plt.xlabel('CoC')
-plt.ylabel('Close')
-plt.show()
+# plt.scatter(df['MA'], df['Close'], color='g')
+# plt.title('Close vs MA')
+# plt.xlabel('MA')
+# plt.ylabel('Close')
+# plt.show()
+
+# plt.scatter(df['High'].shift(1), df['Close'], color='g')
+# plt.title('Close vs MA')
+# plt.xlabel('MA')
+# plt.ylabel('Close')
+# plt.show()
